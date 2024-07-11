c735d71efc2d4f5ace044e98a22f6449cab3b43d
Oleksandr Doroshkevich <145459470+MaskonDivais@users.noreply.github.com>
2024-07-11 15:16:58 +0200

    Tablet

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const Vert = document.querySelectorAll('.Vertika');
     const Hori = document.querySelectorAll('.Horison');
     const HoriLef = document.querySelectorAll('.HorisonLeft');
+    const HoriLefList = document.querySelectorAll('.HoriLefList');
 
     const observerOptions = {
+        threshold: 0.1
+    };
+    const observerOptionsList = {
         threshold: 0.1
     };
 
@@ -15,6 +19,20 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }, observerOptions);
+
+    const observerlist = new IntersectionObserver((entries, observerlist) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('list-show');
+            } else {
+                entry.target.classList.remove('list-show');
+            }
+        });
+    }, observerOptionsList);
+
+    HoriLefList.forEach(HoriLefList => {
+        observerlist.observe(HoriLefList);
+    });
 
     Vert.forEach(Vert => {
         observer.observe(Vert);
